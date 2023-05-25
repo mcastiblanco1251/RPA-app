c34cc8ab0e59a0682fba05862462cffd52fcae8e
Manuel Castiblanco <71740021+mcastiblanco1251@users.noreply.github.com>
2023-05-25 09:58:28 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,53 @@
 import streamlit as st
 import requests
 from translate import Translator
+from PIL import Image
+
+im = Image.open("log.png")
+
+st.set_page_config(page_title='ChatBot', layout="wide", page_icon=im)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# LAYING OUT THE TOP SECTION OF THE APP
+
+row1_1, row1_2 = st.columns((1,3))
+
+with row1_1:
+    image = Image.open('bot.png')
+    ni=image.resize((250, 220))
+    st.image(ni)#, use_column_width=True)
+    st.markdown('Web App by [Manuel Castiblanco](http://ia.smartecorganic.com.co/index.php/contact/)')
+with row1_2:
+    st.write("""
+    # ChatBot
+    Esta app ilustra chatbot!
+    """)
+    with st.expander("Contact us 👉"):
+        with st.form(key='contact', clear_on_submit=True):
+            name=st.text_input('Nombre')
+            mail = st.text_input('Email')
+            q=st.text_area("Consulta")
+
+            submit_button = st.form_submit_button(label='Enviar')
+            if submit_button:
+                subject = 'Consulta'
+                to = 'macs1251@hotmail.com'
+                sender = 'macs1251@hotmail.com'
+                smtpserver = smtplib.SMTP("smtp-mail.outlook.com",587)
+                user = 'macs1251@hotmail.com'
+                password = '1251macs'
+                smtpserver.ehlo()
+                smtpserver.starttls()
+                smtpserver.ehlo()
+                smtpserver.login(user, password)
+                header = 'To:' + to + '\n' + 'From: ' + sender + '\n' + 'Subject:' + subject + '\n'
+                message = header + '\n'+name + '\n'+mail+'\n'+ q
+                smtpserver.sendmail(sender, to, message)
+                smtpserver.close()
+
+
+st.header('Aplicación')
+st.write('_______________________________________________________________________________________________________')
 
 
 def chat_bot(message):
@@ -15,7 +62,7 @@ def chat_bot(message):
     return response.json()['cnt']
 
 def main():
-    st.title("Chat Bot")
+    #st.title("Chat Bot")
     message = st.text_input("Ingrese su mensaje")
     if st.button("Enviar"):
 
